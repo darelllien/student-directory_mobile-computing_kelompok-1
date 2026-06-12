b7b3cc40f98b47c6ba9e6b159f74173dbf6fdaf7
darelllien <darelldamiri04@gmail.com>
2026-06-12 09:34:19 +0700

    feat: add student model and initial app data

---
+++ lib/models/student.dart
@@ -0,0 +1,15 @@
+class Student {
+  final String id;
+  final String name;
+  final String avatar;
+  final String domisili;
+  final String phone;
+
+  Student({
+    required this.id,
+    required this.name,
+    required this.avatar,
+    required this.domisili,
+    required this.phone,
+  });
+}
