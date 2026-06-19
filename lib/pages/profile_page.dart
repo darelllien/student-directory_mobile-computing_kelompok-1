05b9db347cd2cab5e5ffb242a06dfbc6860b25a0
Levanttt <luthfiardi66@gmail.com>
2026-06-19 08:19:08 +0700

    adjust value shadow

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -243,10 +243,10 @@ class _DeleteAccountButton extends StatelessWidget {
             borderRadius: BorderRadius.circular(16),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.15),
+                color: Colors.black.withValues(alpha: 0.15),
                 blurRadius: 20,
                 offset: const Offset(0, 8),
-              ),
+              )
             ],
           ),
           child: Padding(
